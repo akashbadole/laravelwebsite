62d6ff72c6bd594360715a8a0a198ca1f26750d2
unknown <akashbadole@rediffmail.com>
2020-04-28 10:53:41 +0530

    output image and md changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Making website using laravel
+Output:
+![bootstrap](https://raw.githubusercontent.com/akashbadole/bootstrap-bootwatch/master/home_page.png)
--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -2,6 +2,6 @@
   <div class="container">
     <h1>Welcome To Our Site</h1>
     <p class="lead">Welcome to our brand new Laravel powered website. This site uses Laravel version 7.x</p>
-    <p>dummyPrestasse se del fu e di sua le pregato discerniamo audaci, esperienza sí durare che nostri sí, segreto furono pietosa.</p>
+    <p>With my the utters my stood lamplight if distinctly implore entrance. And shore i but cushioned, raven i sent agreeing mortals, the out with fancy wandering on. Radiant hopes then sat reply than devil betook.</p>
   </div>
 </div>
